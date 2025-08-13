04e968325176aa9032117d122e57a98ada678198
Zimon23 <bbeehong2@gmail.com>
2025-08-13 07:58:46 +0000

    run.sh is updated

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
- #/bin/sh
-      for i in {1..6}; do curl -s localhost:8000 | grep hostname; sleep 1; done
+#!/bin/bash
+# run.sh - Docker Swarm 로드밸런싱 테스트용
 
-      chmod +x run.sh 
-      
+# 반복 횟수
+REPEAT=10
+# 요청을 보낼 URL (포트는 서비스에서 publish한 포트로 설정)
+URL="http://localhost:8000"
+
+echo "=== 로드밸런싱 테스트 시작 ==="
+for i in $(seq 1 $REPEAT); do
+    RESPONSE=$(curl -s $URL)
+    HOSTNAME=$(echo $RESPONSE | grep -oP '"hostname":"\K[^"]+')
+    echo "[$i] 응답 컨테이너: $HOSTNAME"
+    sleep 1
+done
+echo "=== 테스트 종료 ==="
